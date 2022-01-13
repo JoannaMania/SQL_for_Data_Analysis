6b598f832acba0d803b8a23b6ffb4ff5651b3aa0
Joanna <50702107+JoannaMania@users.noreply.github.com>
2022-01-13 20:13:57 +0100

    Update funnel_script_warby_parker.sql

--- Analyze real data with sql/Usage funnel with Warby Parker/funnel_script_warby_parker.sql
+++ Analyze real data with sql/Usage funnel with Warby Parker/funnel_script_warby_parker.sql
@@ -1,3 +1,4 @@
+-- PART 1
 -- What columns does the table have?
 SELECT *
 FROM survey
@@ -8,23 +9,28 @@ SELECT question, COUNT(DISTINCT user_id) AS no_of_anwsers
 FROM survey
 GROUP BY question;
 
--- Which question(s) of the quiz have a lower completion rates?
 
-\* 
+/* 
+Which question(s) of the quiz have a lower completion rates?
+
 100% - What are you looking for? 
 95% - What's your fit? 
 80% - Which shapes do you like?
 95% - Which colors do you like?
-74.8% - What do you think is the reason? */
+74.8% - What do you think is the reason? 
 
--- What do you think is the reason?
--- People might not recall very well when was their last check up.
--- Maybe customers might not know what shapes are available
+What do you think is the reason?
+- Customers do not recall very well when was their last check up.
+- Customers are undecisive when it comes to shapes.
+*/
 
-
--- A/B testing. Let’s find out whether or not users who get more pairs to try on at home will be more likely to make a purchase.50% of the users will get 3 pairs to try on and 50% of the users will get 5 pairs to try on. 
+/* 
+PART 2 - A/B testing
+Let’s find out whether or not users who get more pairs to try on at home will be more likely to make a purchase.
+50% of the users will get 3 pairs to try on and 50% of the users will get 5 pairs to try on. 
 
 -- Check the column names
+*/
 
 -- quiz
 -- user_id, style, fit, shape, color
@@ -44,7 +50,7 @@ SELECT *
 FROM purchase
 LIMIT 5;
 
--- join tables 
+-- Join tables to have all information in one place 
 SELECT DISTINCT q.user_id,
    h.user_id IS NOT NULL AS 'is_home_try_on',
    h.number_of_pairs,
@@ -57,7 +63,7 @@ LEFT JOIN purchase p
 LIMIT 10;
 
 
--- Creates a temporary table 
+-- Makes a temporary table 
 WITH funnel AS (
 SELECT DISTINCT q.user_id,
    h.user_id IS NOT NULL AS 'is_home_try_on',
@@ -70,11 +76,19 @@ LEFT JOIN purchase p
    ON p.user_id = q.user_id)
 
  
-SELECT COUNT(user_id) AS quiz, -- Aggregates across the rows
+SELECT number_of_pairs, COUNT(user_id) AS quiz, -- Aggregates across the rows
   SUM(is_home_try_on) AS 'num_checked_out', 
   SUM(is_purchase) AS 'num_purchased', 
-  1.0* SUM(is_home_try_on)/ COUNT(user_id) AS 'quiz_to_check_out', -- Calculates the conversion rates between the steps. 
-  1.0* SUM(is_purchase)/ SUM(is_home_try_on) AS 'check_out_to_purchase'
+  ROUND(1.0* SUM(is_home_try_on)/ COUNT(user_id), 2) AS 'quiz_to_check_out', -- Calculates the conversion rates between the steps. 
+  ROUND(1.0* SUM(is_purchase)/ SUM(is_home_try_on), 2) AS 'check_out_to_purchase'
 FROM funnel
 WHERE number_of_pairs IS NOT NULL -- CalculateS the difference in purchase rates between customers who had 3 number_of_pairs with ones who had 5.
 GROUP BY number_of_pairs; 
+
+/*
+number_of_pairs	quiz	num_checked_out	num_purchased	quiz_to_check_out	check_out_to_purchase
+3 pairs	         379	379	            201	         1.0	            0.53
+5 pairs	         371	371	            294	         1.0	            0.79
+
+Customers who received 5 pairs to try are more inclined to purchase glasses. 
+*/
