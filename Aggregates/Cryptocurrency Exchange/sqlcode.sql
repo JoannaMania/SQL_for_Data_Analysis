85156504a7521378bd950e820387a1609e724a78
Joanna <50702107+JoannaMania@users.noreply.github.com>
2022-01-13 16:07:00 +0100

    Add files via upload

---
+++ Aggregates/Cryptocurrency Exchange/sqlcode.sql
@@ -0,0 +1,43 @@
+-- What is the total money_in in the table?
+SELECT SUM(money_in)
+FROM transactions;
+
+
+-- What is the total money_out in the table?
+SELECT SUM(money_out)
+FROM transactions;
+
+
+-- It was reported that Bitcoin dominates Fiddy Cent’s exchange. Let’s see if it is true within these dates by answering two questions:
+-- How many money_in transactions are in this table? 40
+
+SELECT COUNT(money_in)
+FROM transactions;
+
+-- How many money_in transactions are in this table where ‘BIT’ is the currency? 21
+SELECT COUNT(money_in)
+FROM transactions
+WHERE currency = 'BIT';
+
+
+-- What was the largest transaction in this whole table?
+SELECT MAX(money_in)
+FROM transactions;
+6000.0
+
+SELECT MAX(money_out)
+FROM transactions;
+15047.0
+
+---What is the average money_in in the table for the currency Ethereum (‘ETH’)?
+SELECT AVG(money_in)
+FROM transactions
+WHERE currency = 'ETH'
+131.888888888889
+
+---Let’s build a ledger for the different dates.
+SELECT date, 
+  ROUND(AVG(money_in), 2), 
+  ROUND(AVG(money_out), 2)
+FROM transactions
+GROUP BY date;
